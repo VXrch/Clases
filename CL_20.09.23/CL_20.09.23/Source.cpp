4e3b8f4d365e087e57d7cfa417cd78c34a02f671
Alina <nika23888@gmail.com>
2023-09-22 00:57:22 +0300

    Home work with recursive functions for 22.09.23

--- CL_20.09.23/CL_20.09.23/Source.cpp
+++ CL_20.09.23/CL_20.09.23/Source.cpp
@@ -75,18 +75,48 @@ void task_1() {
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+int Stars_output(int i, int number) {
+
+    i++;
+
+    if (i <= number)
+        cout << " * ";
+
+    if (i > number)
+        return 0;
+
+    return Stars_output(i, number);
+}
+
 void task_2() {
 
     // 3. Написати рекурсивну функцію, яка виводить N зірок
-    //    у ряд, число N задає користувач.Проілюструйте ро -
+    //    у ряд, число N задає користувач. Проілюструйте ро -
     //    боту функції прикладом.
 
+    int i = 0, number_of_stars;
 
+    cout << "Example:\nIf number of stars = 5: " << endl;
+    Stars_output(i, 5); 
+    
+    cout << "\nEnter number of stars: "; cin >> number_of_stars;
 
-
+    if (number_of_stars > 0)
+        Stars_output(i, number_of_stars);
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+int SumOfAllNumbers(int r1, int r2, int i, int sum = 0) {
+    
+    sum += r1;
+    r1++;
+
+    if (r1 > r2)
+        return sum;
+
+    return SumOfAllNumbers(r1, r2, i, sum);
+}
 
 void task_3() {
 
@@ -94,12 +124,42 @@ void task_3() {
     //     всіх чисел у діапазоні від a до b.Користувач вводить
     //     a і b.Проілюструйте роботу функції прикладом.
 
+    int sum, r1, r2, i = 0;
 
+    sum = SumOfAllNumbers(1, 10, i);
+    cout << "For example:\nIn range (1 - 10) sum of all numbers = " << sum << endl;
 
+    cout << "Enter range (min): "; cin >> r1;
+    cout << "Enter range (max): "; cin >> r2;
 
+    if (r1 > 0 && r1 < r2)
+        sum = SumOfAllNumbers(r1, r2, i);
+
+    cout << "Sum of all numbers from " << r1 << " to " << r2 << " = " << sum << endl;
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+int findMinSumPosition(int arr[], int startIndex = 0, int currentSum = 0, int minSum = 2147483647, int minIndex = 0) {
+
+    if (startIndex == 90) {
+        return minIndex;
+    }
+
+    int newSum = currentSum + arr[startIndex];
+
+    if (startIndex >= 10) {
+        newSum -= arr[startIndex - 10];
+    }
+
+    if (newSum < minSum) {
+
+        minSum = newSum;
+        minIndex = startIndex - 9;
+    }
+
+    return findMinSumPosition(arr, startIndex + 1, newSum, minSum, minIndex);
+}
 
 void task_4() {
 
@@ -108,9 +168,13 @@ void task_4() {
     //     вим чином, і знаходить позицію, з якої починається
     //     послідовність з 10 чисел, сума яких мінімальна.
 
+    const int size = 100;
+    int arr[size];
+    Fill(arr, size);
 
+    int minSumIndex = findMinSumPosition(arr);
 
-
+    cout << "Result = " << minSumIndex << endl;
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -123,7 +187,6 @@ void main() {
     int arr[size];
 
     Fill(arr, size);
-    Print_1(arr, size);
 
     int choice = 0;
 
